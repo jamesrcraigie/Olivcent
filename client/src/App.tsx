07387c9467de9e7268dbb6ec734b6092724e2bde
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-09-16 16:13:43 +0100

    updated the App.tsx  to have a basic structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import { Product } from "./product";
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const addProduct = () => {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        id: prevState.length + 1,
+        name: "product" + (prevState.length + 1),
+        price: prevState.length * 10,
+        description: "product" + (prevState.length + 1),
+        pictureUrl:
+          "http://picsum.photos/200" + (prevState.length + 1) + ".png",
+        type: "product" + (prevState.length + 1),
+        quantityInStock: 10,
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data: Product[]) => setProducts(data))
+      .catch((error) =>
+        console.error("There was a problem fetching products:", error)
+      );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <div className="title-header">
+        <h1>Olivcent</h1>
+      </div>
+      <header id="header">
+        <div className="header-nav">
+          <a href="#products">Products</a>
+          <a href="#orders">Orders</a>
+          <a href="#customers">Customers</a>
+          <a href="#about">About</a>
+          <a href="#settings">Settings</a>
+        </div>
       </header>
+      <main id="main">
+        <section>
+          <h2>Welcome to Olivcent Dashboard</h2>
+          <p>Select an option from the header to get started.</p>
+        </section>
+        <section id="products-section">
+          <h2>Products</h2>
+          <button onClick={addProduct}>Add Product</button>
+          <ul>
+            {products.map((product) => (
+              <li key={product.id}>
+                <img src="http://picsum.photos/160" alt={product.name} />
+                {product.name} - £{product.price}
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section id="shopify-section-template">
+          <h2>Shopify Section Template</h2>
+          <a href="#placeholder1">Placeholder Link 1</a>
+          <a href="#placeholder2">Placeholder Link 2</a>
+          <a href="#placeholder3">Placeholder Link 3</a>
+        </section>
+      </main>
     </div>
   );
 }
