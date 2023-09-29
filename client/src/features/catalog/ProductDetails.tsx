819da54ca71ece80fdfb3658c4407598dbe78361
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-09-29 17:25:05 +0100

    feat: items and their content now render when view is pressed

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,5 +1,96 @@
-import { Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Product } from "../../app/models/product"; // Adjust the path based on your file structure
+import axios from "axios";
+import {
+  Grid,
+  Divider,
+  Typography,
+  TableContainer,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "@mui/material";
 
-export default function ProductDetails() {
-  return <Typography variant="h2">This is the product page</Typography>;
+function ProductDetails() {
+  const { id } = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios
+      .get<Product>(`http://localhost:5000/api/products/${id}`)
+      .then((response) => {
+        setProduct(response.data);
+        setError(null);
+      })
+      .catch((error) => {
+        console.error("Error fetching product details:", error);
+        setError(error.message);
+      });
+  }, [id]);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Grid container spacing={6} style={{ paddingTop: "20px" }}>
+      <Grid item xs={6}>
+        <img
+          src={product.pictureUrl}
+          alt={product.name}
+          style={{ width: "85%" }}
+        />
+      </Grid>
+
+      <Grid item xs={6}>
+        <Typography variant="h4" component="h2">
+          {product.name}
+        </Typography>
+        <Divider sx={{ mb: 2, mt: 2 }} />
+        <Typography variant="h4" color="secondary">
+          £{(product.price / 100).toFixed(2)}
+        </Typography>
+
+        <TableContainer style={{ marginTop: "16px" }}>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>{product.name}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Description</TableCell>
+                <TableCell>{product.description}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Type</TableCell>
+                <TableCell>{product.type}</TableCell>
+              </TableRow>
+              {/* <TableRow>
+            <TableCell>Brand</TableCell>
+            <TableCell>{product.brand}</TableCell>
+          </TableRow> */}
+              <TableRow>
+                <TableCell>Quantity in stock</TableCell>
+                <TableCell>{product.quantityInStock}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        <Typography variant="body1" style={{ marginTop: "16px" }}>
+          {product.description}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 }
+
+export default ProductDetails;
